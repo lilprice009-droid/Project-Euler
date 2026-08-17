cbea3016a26288d2ae2718ccb22f3ed92d2a4354
lilprice009-droid <lilprice009@gmail.com>
2026-08-17 21:07:39 +0100

    rage quit

--- challenge2.py
+++ challenge2.py
@@ -1,13 +1,3 @@
-total = 0
-fib = 1
-def fibonacci(num):
-    if num< 400:
-        print(total)
-        total = total + fib
-        fib = total
-
-
-
 
 
     
--- challenge3.py
+++ challenge3.py
@@ -1,11 +1,4 @@
-def prime_check(num):
-    num1 = True
-    for i in  range(2,num):
-        if num1 % i == 0:
-            num1 = False
-            return False
-    return True
-print(prime_check(4))
+
 
     
     
